e9f37e469014ac0bf099b5bcb6b7baaa98830b77
Gabriel LAZA <gabriellaza02@gmail.com>
2023-11-10 14:35:50 +0200

    question:?

--- lunatic-api/Lunatic.Domain/Entities/Team.cs
+++ lunatic-api/Lunatic.Domain/Entities/Team.cs
@@ -10,7 +10,8 @@ namespace Lunatic.Domain.Entities {
 
         public Guid Id { get; private set; }
         public string Name { get; private set; }
-        public List<Tuple<User, TeamRole>>? Users { get; private set; }
+        public List<User>? Members { get; private set; }
+        // public Dictionary<User, TeamRole>? Members { get; private set; }
         public List<Project>? Projects { get; private set; }
 
         public static Result<Team> Create(string name) {
@@ -21,12 +22,20 @@ namespace Lunatic.Domain.Entities {
             return Result<Team>.Success(new Team(name));
         }
 
-        public void AddUser(User user, TeamRole teamRole) {
-            if(Users == null) {
-                Users = new List<Tuple<User, TeamRole>>();
+        // public void AddMember(User user, TeamRole teamRole) {
+        //     if(Members == null) {
+        //         Members = new Dictionary<User, TeamRole>();
+        //     }
+        //
+        //     // hm...
+        //     Members.TryAdd(user, teamRole);
+        // }
+        public void AddUser(User user) {
+            if(Members == null) {
+                Members = new List<User>();
             }
 
-            Users.Add(Tuple.Create(user, teamRole));
+            Members.Add(user);
         }
 
         public void AddProject(Project project) {
