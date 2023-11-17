310590941383ff588ea43efdda741581dacd0ffb
Gabriel LAZA <gabriellaza02@gmail.com>
2023-11-17 17:05:33 +0200

    fix:

--- lunatic-api/Lunatic.Application/Features/Users/Commands/CreateUser/CreateUserComand.cs
+++ lunatic-api/Lunatic.Application/Features/Users/Commands/CreateUser/CreateUserComand.cs
@@ -5,11 +5,11 @@ using MediatR;
 
 namespace Lunatic.Application.Features.Users.Commands.CreateUser {
     public class CreateUserComand : IRequest<CreateUserCommandResponse> {
-        public string FirstName { get; private set; } = default!;
-        public string LastName { get; private set; } = default!;
-        public string Email { get; private set; } = default!;
-        public string Username { get; private set; } = default!;
-        public string Password { get; private set; } = default!;
-        public Role Role { get; private set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Username { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public Role Role { get; set; }
     }
 }
