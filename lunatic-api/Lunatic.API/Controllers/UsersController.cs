98f616a7d4e97246203ab51d8ba8c02d2e241d49
Gabriel LAZA <gabriellaza02@gmail.com>
2023-11-17 11:08:19 +0200

    feat: add user controller

---
+++ lunatic-api/Lunatic.API/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+using Lunatic.Application.Features.Users.Commands.CreateUser;
+using Lunatic.Application.Features.Users.Commands.UpdateUser;
+using Lunatic.Application.Features.Users.Commands.DeleteUser;
+using Lunatic.Application.Features.Users.Queries.GetAll;
+using Lunatic.Application.Features.Users.Queries.GetById;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lunatic.API.Controllers {
+    public class UsersController : ApiControllerBase {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(CreateUserComand command) {
+            var result = await Mediator.Send(command);
+            if (!result.Success) {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> Update(UpdateUserCommand command) {
+            var result = await Mediator.Send(command);
+            if (!result.Success) {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Delete(DeleteUserComand command) {
+            var result = await Mediator.Send(command);
+            if (!result.Success) {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll() {
+            var result = await Mediator.Send(new GetAllUsersQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get(Guid id) {
+            var result = await Mediator.Send(new GetByIdUserQuery(id));
+            return Ok(result);
+        }
+    }
+}
+
