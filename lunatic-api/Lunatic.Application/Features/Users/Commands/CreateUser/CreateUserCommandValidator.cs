5ae1f60c60da3574c628e7f5f244ac2c0609cc15
Gabriel LAZA <gabriellaza02@gmail.com>
2023-11-17 10:23:27 +0200

    feat: add validation on create user

--- lunatic-api/Lunatic.Application/Features/Users/Commands/CreateUser/CreateUserCommandValidator.cs
+++ lunatic-api/Lunatic.Application/Features/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -17,6 +17,10 @@ namespace Lunatic.Application.Features.Users.Commands.CreateUser {
                 .NotNull()
                 .MaximumLength(50).WithMessage("Username must not exceed 50 characters")
                 .Must(UniqueUsername).WithMessage("This username is already taken");
+            RuleFor(p => p.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .NotNull()
+                .MaximumLength(30).WithMessage("Password must not exceed 30 characters");
         }
 
         private bool UniqueUsername(string username) {
