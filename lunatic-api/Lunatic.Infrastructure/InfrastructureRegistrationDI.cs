e46bf360df377708f0006d7046a0232b6ecfcffa
Gabriel LAZA <gabriellaza02@gmail.com>
2023-11-17 11:08:09 +0200

    feat: add service infrastructure

---
+++ lunatic-api/Lunatic.Infrastructure/InfrastructureRegistrationDI.cs
@@ -0,0 +1,32 @@
+
+using Lunatic.Application.Persistence;
+using Lunatic.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Lunatic.Infrastructure {
+    public static class InfrastructureRegistrationDI {
+        public static IServiceCollection AddInfrastrutureToDI(
+            this IServiceCollection services,
+            IConfiguration configuration) {
+            services.AddDbContext<LunaticContext>(
+                options =>
+                options.UseNpgsql(
+                    configuration.GetConnectionString
+                    ("LunaticConnection"),
+                    builder =>
+                    builder.MigrationsAssembly(
+                        typeof(LunaticContext)
+                        .Assembly.FullName)));
+
+            services.AddScoped
+                (typeof(IAsyncRepository<>),
+                typeof(RepositoryBase<>));
+            services.AddScoped
+                <IUserRepository, UserRepository>();
+            return services;
+        }
+    }
+}
