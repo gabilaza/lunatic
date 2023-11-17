8eaed6b4d0b6c5436cfc3da10c42e43a9ba24b42
Gabriel LAZA <gabriellaza02@gmail.com>
2023-11-17 11:08:29 +0200

    fix: api main program

--- lunatic-api/Lunatic.API/Program.cs
+++ lunatic-api/Lunatic.API/Program.cs
@@ -1,6 +1,12 @@
+using Lunatic.Application;
+using Lunatic.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastrutureToDI(
+        builder.Configuration);
+builder.Services.AddApplicationServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,8 +16,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if(app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
