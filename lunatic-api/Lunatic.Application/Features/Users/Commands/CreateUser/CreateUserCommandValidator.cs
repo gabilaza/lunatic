cf053f4fd8990856cdb1515bfc3e3146d8c2aab7
Gabriel LAZA <gabriellaza02@gmail.com>
2023-11-17 10:27:01 +0200

    fix: some spaces

--- lunatic-api/Lunatic.Application/Features/Users/Commands/CreateUser/CreateUserCommandValidator.cs
+++ lunatic-api/Lunatic.Application/Features/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -12,11 +12,13 @@ namespace Lunatic.Application.Features.Users.Commands.CreateUser {
             this.userRepository = userRepository;
 
             RuleFor(p => p.Role).NotNull();
+
             RuleFor(p => p.Username)
                 .NotEmpty().WithMessage("Username is required")
                 .NotNull()
                 .MaximumLength(50).WithMessage("Username must not exceed 50 characters")
                 .Must(UniqueUsername).WithMessage("This username is already taken");
+
             RuleFor(p => p.Password)
                 .NotEmpty().WithMessage("Password is required")
                 .NotNull()
