741a09fd80860456b33d21b8463df4e167e996ab
theDotAfterExe <valentin.filip@student.uaic.ro>
2023-12-04 22:57:36 +0200

    added validation rules

--- lunatic-api/Lunatic.Infrastructure/Repositories/User/UserRepository.cs
+++ lunatic-api/Lunatic.Infrastructure/Repositories/User/UserRepository.cs
@@ -2,6 +2,7 @@
 using Lunatic.Application.Persistence;
 using Lunatic.Domain.Entities;
 using Lunatic.Domain.Utils;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace Lunatic.Infrastructure.Repositories {
@@ -10,20 +11,22 @@ namespace Lunatic.Infrastructure.Repositories {
         }
 
         public async Task<Result<User>> FindByUsernameAsync(string username) {
-            var result = await context.Set<User>().FindAsync(username);
-            if(result == null) {
-                return Result<User>.Failure($"Entity with username {username} not found");
-            }
-            return Result<User>.Success(result);
+			var result = await context.Set<User>().FirstOrDefaultAsync(u => u.Username == username);
+
+			if (result == null) {
+				return Result<User>.Failure($"Entity with username {username} not found");
+			}
+			return Result<User>.Success(result);
         }
 
         public async Task<Result<User>> FindByEmailAsync(string email) {
-            var result = await context.Set<User>().FindAsync(email); //cautare cu predicat + extensie ; first or default + predicat 
-            if(result == null) {
-                return Result<User>.Failure($"Entity with username {email} not found");
-            }
-            return Result<User>.Success(result);
-        }
+			var result = await context.Set<User>().FirstOrDefaultAsync(u => u.Email == email);
+
+			if (result == null) {
+				return Result<User>.Failure($"Entity with username {email} not found");
+			}
+			return Result<User>.Success(result);
+		}
     }
 }
 
