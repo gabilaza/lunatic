853320406b609198eabf31a98461df1d6406da93
theDotAfterExe <valentin.filip@student.uaic.ro>
2023-12-06 23:34:43 +0200

    added broken JWT support

---
+++ lunatic-api/Lunatic.Application/Contracts/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿
+using Lunatic.Application.Models;
+
+namespace Lunatic.Application.Contracts {
+    public interface IEmailService {
+        Task<bool> SendEmailAsync(Mail email);
+    }
+}
--- lunatic-api/Lunatic.Infrastructure/InfrastructureRegistrationDI.cs
+++ lunatic-api/Lunatic.Infrastructure/InfrastructureRegistrationDI.cs
@@ -8,32 +8,22 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Lunatic.Infrastructure {
     public static class InfrastructureRegistrationDI {
-        public static IServiceCollection AddInfrastrutureToDI(
+        public static IServiceCollection AddInfrastructureToDI(
             this IServiceCollection services,
             IConfiguration configuration) {
             services.AddDbContext<LunaticContext>(
-                options =>
-                options.UseNpgsql(
-                    configuration.GetConnectionString
-                    ("LunaticConnection"),
-                    builder =>
-                    builder.MigrationsAssembly(
-                        typeof(LunaticContext)
-                        .Assembly.FullName)));
+                options => options.UseNpgsql(
+                    configuration.GetConnectionString("LunaticConnection"),
+                    builder => builder.MigrationsAssembly(typeof(LunaticContext).Assembly.FullName)
+                    )
+                );
 
-            services.AddScoped
-                (typeof(IAsyncRepository<>),
-                typeof(RepositoryBase<>));
-            services.AddScoped
-                <ICommentRepository, CommentRepository>();
-            services.AddScoped
-                <IProjectRepository, ProjectRepository>();
-            services.AddScoped
-                <ITaskRepository, TaskRepository>();
-            services.AddScoped
-                <ITeamRepository, TeamRepository>();
-            services.AddScoped
-                <IUserRepository, UserRepository>();
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IProjectRepository, ProjectRepository>();
+            services.AddScoped<ITaskRepository, TaskRepository>();
+            services.AddScoped<ITeamRepository, TeamRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
             return services;
         }
     }
