672d132502f6b7c40c07ec67cc3dbeba362a7335
Gabriel LAZA <gabriellaza02@gmail.com>
2023-12-08 09:11:19 +0200

    feat: add task update

---
+++ lunatic-api/Lunatic.Application/Features/Tasks/Commands/UpdateTask/UpdateTaskCommand.cs
@@ -0,0 +1,21 @@
+﻿
+using Lunatic.Domain.Entities;
+using MediatR;
+using TaskStatus = Lunatic.Domain.Entities.TaskStatus;
+
+
+namespace Lunatic.Application.Features.Tasks.Commands.UpdateTask {
+    public class UpdateTaskCommand : IRequest<UpdateTaskCommandResponse> {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public TaskPriority Priority { get; set; } = default!;
+        public TaskStatus Status { get; set; } = default!;
+        public List<Tag> Tags { get; set; } = default!;
+        public List<Guid> CommentIds { get; set; } = default!;
+        public List<Guid> UserAssignIds { get; set; } = default!;
+
+        public DateTime StartedDate { get; set; } = default!;
+        public DateTime EndedDate { get; set; } = default!;
+    }
+}
