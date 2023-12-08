d6253c1fe02ff7681ab9ad1ee5d9fde47aff7896
Gabriel LAZA <gabriellaza02@gmail.com>
2023-12-08 16:48:12 +0200

    coment

--- lunatic-api/Lunatic.Application/Features/Users/Queries/GetById/GetByIdUserQueryHandler.cs
+++ lunatic-api/Lunatic.Application/Features/Users/Queries/GetById/GetByIdUserQueryHandler.cs
@@ -1,39 +1,39 @@
-﻿
-using Lunatic.Application.Persistence;
-using MediatR;
-
-
-namespace Lunatic.Application.Features.Users.Queries.GetById {
-    public class GetByIdUserQueryHandler : IRequestHandler<GetByIdUserQuery, GetByIdUserResponse> {
-        private readonly IUserRepository userRepository;
-
-        public GetByIdUserQueryHandler(IUserRepository userRepository) {
-            this.userRepository = userRepository;
-        }
-
-        public async Task<GetByIdUserResponse> Handle(GetByIdUserQuery request, CancellationToken cancellationToken) {
-            var user = await userRepository.FindByIdAsync(request.Id);
-            if(!user.IsSuccess) {
-                return new GetByIdUserResponse {
-                    Success = false,
-                    ValidationErrors = new List<string> { "User not found" }
-                };
-            }
-
-            return new GetByIdUserResponse {
-                Success = true,
-                User = new UserDto {
-                    Id = new Guid(user.Value.Id),
-                    FirstName = user.Value.FirstName,
-                    LastName = user.Value.LastName,
-                    Email = user.Value.Email,
-                    Username = user.Value.UserName,
-                    Password = user.Value.PasswordHash,
-                    Role = user.Value.Role,
-                    Teams = user.Value.Teams
-                }
-            };
-        }
-    }
-}
-
+﻿//
+// using Lunatic.Application.Persistence;
+// using MediatR;
+//
+//
+// namespace Lunatic.Application.Features.Users.Queries.GetById {
+//     public class GetByIdUserQueryHandler : IRequestHandler<GetByIdUserQuery, GetByIdUserResponse> {
+//         private readonly IUserRepository userRepository;
+//
+//         public GetByIdUserQueryHandler(IUserRepository userRepository) {
+//             this.userRepository = userRepository;
+//         }
+//
+//         public async Task<GetByIdUserResponse> Handle(GetByIdUserQuery request, CancellationToken cancellationToken) {
+//             var user = await userRepository.FindByIdAsync(request.Id);
+//             if(!user.IsSuccess) {
+//                 return new GetByIdUserResponse {
+//                     Success = false,
+//                     ValidationErrors = new List<string> { "User not found" }
+//                 };
+//             }
+//
+//             return new GetByIdUserResponse {
+//                 Success = true,
+//                 User = new UserDto {
+//                     Id = new Guid(user.Value.Id),
+//                     FirstName = user.Value.FirstName,
+//                     LastName = user.Value.LastName,
+//                     Email = user.Value.Email,
+//                     Username = user.Value.UserName,
+//                     Password = user.Value.PasswordHash,
+//                     Role = user.Value.Role,
+//                     Teams = user.Value.Teams
+//                 }
+//             };
+//         }
+//     }
+// }
+//
