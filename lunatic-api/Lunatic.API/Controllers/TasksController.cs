4dc2f453a8e7174c0d82f62e07857c93d407850c
Gabriel LAZA <gabriellaza02@gmail.com>
2023-12-08 09:11:52 +0200

    feat: add task controller

---
+++ lunatic-api/Lunatic.API/Controllers/TasksController.cs
@@ -0,0 +1,68 @@
+using Lunatic.Application.Features.Tasks.Commands.CreateTask;
+using Lunatic.Application.Features.Tasks.Commands.DeleteTask;
+using Lunatic.Application.Features.Tasks.Commands.UpdateTask;
+using Lunatic.Application.Features.Tasks.Queries.GetAll;
+using Lunatic.Application.Features.Tasks.Queries.GetById;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lunatic.API.Controllers {
+    public class TasksController : ApiControllerBase {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(CreateTaskComand command) {
+            var result = await Mediator.Send(command);
+            if(!result.Success) {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> Update(Guid id, UpdateTaskCommand command) {
+            var existsResult = await Mediator.Send(new GetByIdTaskQuery(id));
+            if(!existsResult.Success) {
+                return NotFound(existsResult);
+            }
+
+            var result = await Mediator.Send(command);
+            if(!result.Success) {
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Delete(Guid id) {
+            var deleteTaskCommand = new DeleteTaskCommand() { Id = id };
+            var result = await Mediator.Send(deleteTaskCommand);
+            if(!result.Success) {
+                return NotFound(result);
+            }
+            return NoContent();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll() {
+            var result = await Mediator.Send(new GetAllTasksQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(Guid id) {
+            var result = await Mediator.Send(new GetByIdTaskQuery(id));
+            if(!result.Success) {
+                return NotFound(result);
+            }
+            return Ok(result);
+        }
+    }
+}
+
