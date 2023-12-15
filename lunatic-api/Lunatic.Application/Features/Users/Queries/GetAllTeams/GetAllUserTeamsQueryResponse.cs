488413c1c2fc2ed662b4ef8eac02d90225b7f6d8
filipvalentin <valentin.filip@student.uaic.ro>
2023-12-15 17:45:26 +0200

    Update GetAllUserTeamsQueryResponse.cs

--- lunatic-api/Lunatic.Application/Features/Users/Queries/GetAllTeams/GetAllUserTeamsQueryResponse.cs
+++ lunatic-api/Lunatic.Application/Features/Users/Queries/GetAllTeams/GetAllUserTeamsQueryResponse.cs
@@ -1,8 +1,10 @@
 using Lunatic.Application.Features.Teams.Payload;
+using Lunatic.Application.Responses;
 
 
 namespace Lunatic.Application.Features.Users.Queries.GetAllTeams {
-	public class GetAllUserTeamsQueryResponse {
+	public class GetAllUserTeamsQueryResponse : ResponseBase {
+		public GetAllUserTeamsQueryResponse() : base() { }
 		public List<TeamDto> Teams { get; set; } = default!;
 	}
 }
