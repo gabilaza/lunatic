fc682d585807222dae91299134fcf34769a8afb2
Gabriel LAZA <gabriellaza02@gmail.com>
2023-12-20 11:47:25 +0200

    feat: add tests structure

---
+++ lunatic-api/Tests/Lunatic.API.Tests/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace Lunatic.API.Tests;
+
+public class UnitTest1 {
+    [Fact]
+    public void Test1() {
+
+    }
+}
---
+++ lunatic-api/Tests/Lunatic.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace Lunatic.Domain.Tests;
+
+public class UnitTest1 {
+    [Fact]
+    public void Test1() {
+
+    }
+}
