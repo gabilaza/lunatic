168470f9c49919b104f0e007720cc209454fbc64
Gabriel LAZA <gabriellaza02@gmail.com>
2023-12-20 16:58:05 +0200

    task tests

---
+++ lunatic-api/Tests/Lunatic.Domain.Tests/Entities/TaskTests/AddAndRemoveTagTests.cs
@@ -0,0 +1,27 @@
+
+using Lunatic.Domain.Entities;
+using Task = Lunatic.Domain.Entities.Task;
+
+
+namespace Lunatic.Domain.Tests.Entities.TaskTests {
+    public class AddAndRemoveTagTests {
+        public const string Title = "Lunatic";
+        public const string Description = "Lunatic Description";
+        public const TaskPriority Priority = TaskPriority.LOW;
+
+        [Fact]
+        public void GivenTaskWithProject_WhenAddAndRemoveProject_ThenProjectAddedAndRemovedSuccessfully() {
+            // given
+            var task = Task.Create(Guid.NewGuid(), Guid.NewGuid(), Title, Description, Priority).Value;
+            var tag = Tag.DOCUMENTATION;
+
+            // when
+            task.AddTag(tag);
+            task.RemoveTag(tag);
+
+            // then
+            Assert.DoesNotContain(tag, task.Tags);
+        }
+    }
+}
+
