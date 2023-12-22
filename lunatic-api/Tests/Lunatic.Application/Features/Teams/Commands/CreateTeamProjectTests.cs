0817592e2a8d226149ee67da8d349e0de86e28a4
Rusu Vlad <96465609+vladrusu7@users.noreply.github.com>
2023-12-22 12:46:15 +0200

    CreateTeamProject failure tests

--- lunatic-api/Tests/Lunatic.Application/Features/Teams/Commands/CreateTeamProjectTests.cs
+++ lunatic-api/Tests/Lunatic.Application/Features/Teams/Commands/CreateTeamProjectTests.cs
@@ -12,7 +12,7 @@ namespace Tests.Lunatic.Application.Features.Teams.Commands
         private static Result<Team> team = Team.Create(user.Value.UserId, "teamName", "teamDescription");
 
         [Fact]
-		public async void GivenCreateTeamProjectCommand_WhenCreate_Then_SuccessResponse()
+		public async void GivenCreateTeamProjectCommand_WhenCreate_ThenSuccessResponse()
         {
             // given
             var userRepository = Substitute.For<IUserRepository>();
@@ -43,5 +43,131 @@ namespace Tests.Lunatic.Application.Features.Teams.Commands
             Assert.NotNull(response.Project);
             Assert.Null(response.ValidationErrors);
         }
+
+        //[Theory]
+        //[InlineData(new[]("", team.Value.TeamId, "", ""))]
+        [Fact]
+        public async void GivenInvalidUserId_WhenCreate_ThenFailureResponse()
+        {
+			// given
+			var userRepository = Substitute.For<IUserRepository>();
+			var teamRepository = Substitute.For<ITeamRepository>();
+			var projectRepository = Substitute.For<IProjectRepository>();
+
+			userRepository.ExistsByIdAsync(user.Value.UserId).Returns(true);
+			teamRepository.ExistsByIdAsync(team.Value.TeamId).Returns(true);
+			teamRepository.FindByIdAsync(team.Value.TeamId).Returns(team);
+
+            var command = new CreateTeamProjectCommand
+            {
+                TeamId = team.Value.TeamId,
+                UserId = Guid.NewGuid(),
+                Title = "projectTitle",
+                Description = "projectDescription"
+            };
+
+            var commandHandler = new CreateTeamProjectCommandHandler(projectRepository, teamRepository, userRepository);
+
+            var source = new CancellationTokenSource();
+
+            var response = await commandHandler.Handle(command, source.Token);
+
+            Assert.False(response.Success);
+            Assert.Null(response.Project);
+            Assert.NotNull(response.ValidationErrors);
+		}
+
+		[Fact]
+		public async void GivenInvalidTeamId_WhenCreate_ThenFailureResponse()
+		{
+			// given
+			var userRepository = Substitute.For<IUserRepository>();
+			var teamRepository = Substitute.For<ITeamRepository>();
+			var projectRepository = Substitute.For<IProjectRepository>();
+
+			userRepository.ExistsByIdAsync(user.Value.UserId).Returns(true);
+			teamRepository.ExistsByIdAsync(team.Value.TeamId).Returns(true);
+			teamRepository.FindByIdAsync(team.Value.TeamId).Returns(team);
+
+			var command = new CreateTeamProjectCommand
+			{
+				TeamId = Guid.NewGuid(),
+				UserId = user.Value.UserId,
+				Title = "projectTitle",
+				Description = "projectDescription"
+			};
+
+			var commandHandler = new CreateTeamProjectCommandHandler(projectRepository, teamRepository, userRepository);
+
+			var source = new CancellationTokenSource();
+
+			var response = await commandHandler.Handle(command, source.Token);
+
+			Assert.False(response.Success);
+			Assert.Null(response.Project);
+			Assert.NotNull(response.ValidationErrors);
+		}
+
+		[Fact]
+		public async void GivenInvalidTitle_WhenCreate_ThenFailureResponse()
+		{
+			// given
+			var userRepository = Substitute.For<IUserRepository>();
+			var teamRepository = Substitute.For<ITeamRepository>();
+			var projectRepository = Substitute.For<IProjectRepository>();
+
+			userRepository.ExistsByIdAsync(user.Value.UserId).Returns(true);
+			teamRepository.ExistsByIdAsync(team.Value.TeamId).Returns(true);
+			teamRepository.FindByIdAsync(team.Value.TeamId).Returns(team);
+
+			var command = new CreateTeamProjectCommand
+			{
+				TeamId = team.Value.TeamId,
+				UserId = user.Value.UserId,
+				Title = "",
+				Description = "projectDescription"
+			};
+
+			var commandHandler = new CreateTeamProjectCommandHandler(projectRepository, teamRepository, userRepository);
+
+			var source = new CancellationTokenSource();
+
+			var response = await commandHandler.Handle(command, source.Token);
+
+			Assert.False(response.Success);
+			Assert.Null(response.Project);
+			Assert.NotNull(response.ValidationErrors);
+		}
+
+		[Fact]
+		public async void GivenInvalidDescription_WhenCreate_ThenFailureResponse()
+		{
+			// given
+			var userRepository = Substitute.For<IUserRepository>();
+			var teamRepository = Substitute.For<ITeamRepository>();
+			var projectRepository = Substitute.For<IProjectRepository>();
+
+			userRepository.ExistsByIdAsync(user.Value.UserId).Returns(true);
+			teamRepository.ExistsByIdAsync(team.Value.TeamId).Returns(true);
+			teamRepository.FindByIdAsync(team.Value.TeamId).Returns(team);
+
+			var command = new CreateTeamProjectCommand
+			{
+				TeamId = team.Value.TeamId,
+				UserId = user.Value.UserId,
+				Title = "projectTitle",
+				Description = ""
+			};
+
+			var commandHandler = new CreateTeamProjectCommandHandler(projectRepository, teamRepository, userRepository);
+
+			var source = new CancellationTokenSource();
+
+			var response = await commandHandler.Handle(command, source.Token);
+
+			Assert.False(response.Success);
+			Assert.Null(response.Project);
+			Assert.NotNull(response.ValidationErrors);
+		}
 	}
 }
