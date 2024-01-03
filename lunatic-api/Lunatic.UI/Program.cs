0e3dc4b88217582c6e991e794e4aa6bda63ecc2d
filipvalentin <valentin.filip@student.uaic.ro>
2024-01-03 15:24:16 +0200

    snackbar misbehaved

--- lunatic-api/Lunatic.UI/Program.cs
+++ lunatic-api/Lunatic.UI/Program.cs
@@ -46,7 +46,17 @@ namespace Lunatic.UI {
 				client.BaseAddress = new Uri("https://localhost:7555/");
 			});
 
-			builder.Services.AddMudServices();
+			builder.Services.AddMudServices(config => {
+				//config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+				//config.SnackbarConfiguration.PreventDuplicates = false;
+				//config.SnackbarConfiguration.NewestOnTop = false;
+				//config.SnackbarConfiguration.ShowCloseIcon = true;
+				config.SnackbarConfiguration.VisibleStateDuration = 5000;
+				config.SnackbarConfiguration.HideTransitionDuration = 100;
+				config.SnackbarConfiguration.ShowTransitionDuration = 500;
+				//config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+			});
 
 			await builder.Build().RunAsync();
 
